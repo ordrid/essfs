6b88004ec648f308d01b29afeab041f9f60c88a6
Michael B. Espeña <espena.michael.pub@gmail.com>
2024-03-28 10:32:31 +0800

    chapter 6: implement a simple CSV file reader

--- src/Chapter6/Program.fs
+++ src/Chapter6/Program.fs
@@ -1,2 +1,48 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System.IO
+
+type Customer =
+    { CustomerId: string
+      Email: string
+      IsEligible: string
+      IsRegistered: string
+      DateRegistered: string
+      Discount: string }
+
+type DataReader = string -> Result<string seq, exn>
+
+let readFile: DataReader =
+    fun path ->
+        try
+            File.ReadLines(path) |> Ok
+        with ex ->
+            Error ex
+
+let parseLine (line: string) : Customer option =
+    match line.Split('|') with
+    | [| customerId; email; eligible; registered; dateRegistered; discount |] ->
+        Some
+            { CustomerId = customerId
+              Email = email
+              IsEligible = eligible
+              IsRegistered = registered
+              DateRegistered = dateRegistered
+              Discount = discount }
+    | _ -> None
+
+let parse (data: string seq) =
+    data |> Seq.skip 1 |> Seq.map parseLine |> Seq.choose id
+
+let output data =
+    data |> Seq.iter (fun x -> printfn "%A" x)
+
+let import (dataReader: DataReader) path =
+    match path |> dataReader with
+    | Ok data -> data |> parse |> output
+    | Error ex -> printfn "Error: %A" ex.Message
+
+[<EntryPoint>]
+let main argv =
+    Path.Combine(__SOURCE_DIRECTORY__, "resources", "customers.csv")
+    |> import readFile
+
+    0
